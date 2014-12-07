c6126f4e7bb43a45e11b636adc0647afb19606e1
Norma Veronica <nvth28@gmail.com>
2014-12-07 01:23:48 -0600

    sigue agonizando

---
+++ miniProyecto2/builder.cpp
@@ -0,0 +1,613 @@
+#include "composite.cpp"
+/*  Fernández Martínez José Ismael
+    Trinidad Hernández Norma Verónica
+    Vilchis Domínguez Miguel Alonso
+    Práctica 03
+    Oct 2014
+*/
+
+class MASTBuilder:public ASTBuilder{
+public:
+
+
+//*STRING 
+	Node* bSTRINGNode(string v){
+		return new StringNode(v);
+	}
+//IDENTIFICADOR 
+	Node* bIDENTIFICADORNode(string v){
+		return new IdentificadorNode(v);
+	}
+//PRINT 
+	Node* bPRINTNode(NodeList* l){
+		INode* n = new PrintNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+
+	Node* bPRINTNode(Node* c){
+		INode* n = new PrintNode;
+		n->setFChild(c);
+		return n;
+	}
+//FALSE 
+	Node* bFALSENode(){
+		return new FalseNode;
+	}
+//IS 
+	/*Node* bISNode(Node* n1, Node* n2){
+		INode* n = new IsNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bISNode(){
+		return new IsNode;
+	}*/
+//RETURN
+	Node* bRETURNNode(NodeList* l){
+		INode* n = new ReturnNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+	Node* bRETURNNode(){
+		return new ReturnNode;
+	}
+	Node* bRETURNNode(Node* c){
+		INode* n = new ReturnNode;
+		n->setFChild(c);
+		return n;	
+	}
+ 
+//CONTINUE 
+	Node* bCONTINUENode(){
+		return new ContinueNode;
+	}
+//FOR 
+	Node* bFORNode(NodeList* l){
+		INode* n = new ForNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+//TRUE
+	Node* bTRUENode(){
+		return new TrueNode;
+	}
+
+//WHILE
+	Node* bWHILENode(Node* n1, Node* n2){
+		INode* n = new WhileNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//AND 
+	Node* bANDNode(Node* n1, Node* n2){
+		INode* n = new AndNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//DEL
+	Node* bDELNode(Node* c){
+		INode* n = new DelNode;
+		n->setFChild(c);
+		return n;
+	}
+//GLOBAL
+	Node* bGLOBALNode(NodeList* l){
+		INode* n = new GlobalNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+//NOT
+	Node* bNOTNode(Node* c){
+		INode* n = new NotNode;
+		n->setFChild(c);
+		return n;
+	}
+//WITH
+	Node* bWITHNode(Node* c){
+		INode* n = new WithNode;
+		n->setFChild(c);
+		return n;
+	}
+//AS
+	Node* bASNode(Node* c){
+		INode* n = new AsNode;
+		n->setFChild(c);
+		return n;
+	}
+//ELIF 
+	Node* bELIFNode(NodeList* l){
+		INode* n = new ElIfNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+//IF 
+	Node* bIFNode(NodeList* l){
+		INode* n = new IfNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+//OR 
+	Node* bORNode(Node* n1, Node* n2){
+		INode* n = new OrNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//ELSE 
+//PASS
+	Node* bPASSNode(){
+		return new PassNode;
+	}
+//BREAK
+	Node* bBREAKNode(){
+		return new BreakNode;
+	}
+//IN 
+//EXEC
+	Node* bEXECNode(NodeList* l){
+		INode* n = new FuncionNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+//NIGUAL
+	Node* bNIGNode(Node* n1, Node* n2){
+		INode* n = new NIgNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bNIGNode(){
+		return new NIgNode;
+	}
+//DIGUAL
+	Node* bDIGNode(Node* n1, Node* n2){
+		INode* n = new DIgNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bDIGNode(){
+		return new DIgNode;
+	}
+//MAIGUAL
+	Node* bMAYIGNode(Node* n1, Node* n2){
+		INode* n = new MayIgNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bMAYIGNode(){
+		return new MayIgNode;
+	}
+//MEIGUAL
+	Node* bMENIGNode(Node* n1, Node* n2){
+		INode* n = new MenIgNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bMENIGNode(){
+		return new MenIgNode;
+	}
+//MAYOR
+	Node* bMAYORNode(Node* n1, Node* n2){
+		INode* n = new MayorNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bMAYORNode(){
+		return new MayorNode;
+	}
+//MENOR
+	Node* bMENORNode(Node* n1, Node* n2){
+		INode* n = new MenorNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bMENORNode() {
+	return new MenorNode;
+	}
+//ENETILDE
+	Node* bENETILDENode(Node* c){
+		INode* n = new EnetildeNode;
+		n->setFChild(c);
+		return n;
+	}
+//CIRCUNFLEjo
+	Node* bCIRCUNFLEJONode(Node* n1, Node* n2){
+		INode* n = new CircunflejoNode;
+		n->setFChild(n1);
+		n->setFChild(n2);
+		return n;
+	}
+//PIPE
+	Node* bPIPENode(Node* n1, Node* n2){
+		INode* n= new PipeNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//AMPERSON
+	Node* bAMPERSONNode(Node* n1, Node* n2){
+		INode* n= new AmpersonNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//DMAYOR
+	Node* bDMAYORNode(Node* n1, Node* n2){
+		INode* n= new DMayorNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//DMENOR
+	Node* bDMENORNode(Node* n1, Node* n2){
+		INode* n= new DMenorNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//PORCEN
+	Node* bMODNode(Node* n1, Node* n2){
+		INode* n= new ModNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bMODNode(){
+		return new ModNode;
+	}
+//DDIAG
+	Node* bDDIAGNode(Node* n1, Node* n2){
+		INode* n= new DDiagNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bDDIAGNode(){
+		return new DDiagNode;
+	}
+//DIAG
+	Node* bDIVNode(Node* n1, Node* n2){
+		INode* n = new DivNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bDIVNode(){
+		return new DivNode;
+	}
+//DASTERISCO
+	Node* bDASTERISTICOSNode(Node* n1, Node* n2){
+		INode* n = new DAsteriscosNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+//ASTERISCO
+	Node* bPORNode(Node* n1, Node* n2){
+		INode* n = new PorNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bPORNode(){
+		return new PorNode;
+	}
+//MENOS
+	Node* bMENOSNode(Node* n1, Node* n2){
+		INode* n = new MenosNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bMENOSNode(){
+		return new MenosNode;
+	}
+//MAS
+	Node* bMASNode(Node* n1, Node* n2){
+		INode* n = new MasNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bMASNode(){
+		return new MasNode;
+	}
+//APAREN
+	Node* bAPARENNode(){
+		return new AParenNode;
+	}
+//CPAREN
+	Node* bCPARENNode(){
+		return new CParenNode;
+	}
+//ACORCHETE
+	Node* bACORCHETENode(){
+		return new ACorcheteNode;
+	}
+//CCORCHETE
+	Node* bCCORCHETENode(){
+		return new CCorcheteNode;
+	}
+//ALLAVE
+	Node* bALLAVENode(){
+		return new ALlaveNode;
+	}
+//CLLAVE
+	Node* bCLLAVENode(){
+		return new CLlaveNode;
+	}
+//COMA
+	Node* bCOMANode(){
+		return new ComaNode;
+	}
+//DPUNTO
+	Node* bDPUNTONode(){
+		return new DPuntoNode;
+	}
+//PUNTO
+	Node* bPUNTONode(){
+		return new PuntoNode;
+	}
+//PCOMA
+	Node* bPCOMANode(){
+		return new PComaNode;
+	}
+//IGUAL
+	Node* bIGUALNode(Node* n1, Node* n2){
+		INode* n = new IgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bIGUALNode(){
+		return new IgualNode;
+	}
+//MASIGUAL
+	Node* bMASIGUALNode(Node* n1, Node* n2){
+		INode* n = new MasIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bMASIGUALNode(){
+		return new MasIgualNode;
+	}
+//MENOSIGUAL
+	Node* bMENOSIGUALNode(Node* n1, Node* n2){
+		INode* n = new MenosIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bMENOSIGUALNode(){
+		return new MenosIgualNode;
+	}
+//ASTIGUAL
+	Node* bASIGUALNode(Node* n1, Node* n2){
+		INode* n = new AsIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bASIGUALNode(){
+		return new AsIgualNode;
+	}
+//DIAIGUAL
+	Node* bDIAIGUALNode(Node* n1, Node* n2){
+		INode* n = new DiaIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bDIAIGUALNode(){
+		return new DiaIgualNode;
+	}
+//DDIAIGUAL
+	Node* bDDIAIGUALNode(Node* n1, Node* n2){
+		INode* n = new DDiaIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bDDIAIGUALNode(){
+		return new DDiaIgualNode;
+	}
+//PORIGUAL
+	Node* bPORIGUALNode(Node* n1, Node* n2){
+		INode* n = new PorIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bPORIGUALNode(){
+		return new PorIgualNode;
+	}
+//AMPIGUAL
+	Node* bAMPIGUALNode(Node* n1, Node* n2){
+		INode* n = new AmpIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bAMPIGUALNode(){
+		return new AmpIgualNode;
+	}
+//PIPEIGUAL
+	Node* bPIPEIGUALNode(Node* n1, Node* n2){
+		INode* n = new PipeIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bPIPEIGUALNode(){
+		return new PipeIgualNode;
+	}
+//CIRCIGUAL
+	Node* bCIRCIGUALNode(Node* n1, Node* n2){
+		INode* n = new CircIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bCIRCIGUALNode(){
+		return new CircIgualNode;
+	}
+//DMAYORIGUAL
+	Node* bDMAYORIGUALNode(Node* n1, Node* n2){
+		INode* n = new DMayorIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bDMAYORIGUALNode(){
+		return new DMayorIgualNode;
+	}
+//DMENORIGUAL
+	Node* bDMENORIGUALNode(Node* n1, Node* n2){
+		INode* n = new DMenorIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bDMENORIGUALNode(){
+		return new DMenorIgualNode;
+	}
+//DASTIGUAL
+	Node* bDASTIGUALNode(Node* n1, Node* n2){
+		INode* n = new DAstIgualNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+
+	Node* bDASTIGUALNode(){
+		return new DAstIgualNode;
+	}
+//MENORMAYOR
+	Node* bMENORMAYORNode(Node* n1, Node* n2){
+		INode* n = new MenorMayorNode;
+		n->setFChild(n1);
+		n->setSChild(n2);
+		return n;
+	}
+	Node* bMENORMAYORNode(){
+		return new MenorMayorNode;
+	}
+//NEWLINE
+	Node* bNEWLINENode(){
+		return new NewLineNode;
+	}
+//*ENTERO 
+	Node* bINTNode(int v){
+		return new IntNode(v);
+	}
+//FLOTANTE
+	Node* bFLOATNode(float v){
+		return new FloatNode(v);
+	}
+//INDENT 
+	Node* bIDENTACIONNode(){
+		return new IdentacionNode;
+	}
+//DEDENT
+	Node* bDENTACIONNode(){
+		return new DentacionNode;
+	}
+//In
+	Node* bINNode(){
+		return new InNode;
+	}
+//notin
+	Node* bNOTINNode(){
+		return new NotInNode;
+	}
+//Is
+	Node* bISNode(){
+		return new IsNode;
+	}
+//isnot
+	Node* bISNOTNode(){
+		return new IsNotNode;
+	}
+	
+	
+	//Node* bBOOLNode(bool v){
+	//	return new BoolNode(v);
+	//}
+	Node* bFUNCIONNode(NodeList* l){
+		INode* n = new FuncionNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+
+	Node* bFUNCIONNode(Node* a, Node* b, Node* c){
+		INode* n = new FuncionNode;
+		n->addSChild(a);
+		n->addSChild(b);
+		n->addSChild(c);
+		return n;
+	}
+	Node* bHERMANOSNode(Node* a, Node* b){
+		INode* n = new HermanosNode;
+		n->addSChild(a);
+		n->addSChild(b);
+		return n;
+	}
+	Node* bHERMANOSNode(Node* a){
+		INode* n = new HermanosNode;
+		n->addSChild(a);
+		return n;
+	}
+	Node* bHERMANOSNode(){
+		INode* n = new HermanosNode;
+		return n;
+	}
+	Node* bHERMANOSNode(NodeList* l){
+		INode* n = new HermanosNode;
+		Niterador *it = l->getIterador();
+		while(it->hasNext())
+			n->addSChild(it->next());
+		return n;
+	}
+
+
+
+};
---
+++ miniProyecto2/pypar.hpp
@@ -0,0 +1,176 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_PYPAR_HPP_INCLUDED
+# define YY_YY_PYPAR_HPP_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+/* "%code requires" blocks.  */
+#line 26 "pypar.y" /* yacc.c:1909  */
+
+#include "composite.cpp"
+
+#line 48 "pypar.hpp" /* yacc.c:1909  */
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    STRING = 258,
+    ESPTAB = 259,
+    OPERADOR = 260,
+    IDENTIFICADOR = 261,
+    PRINT = 262,
+    FALSE = 263,
+    CLASS = 264,
+    FINALLY = 265,
+    IS = 266,
+    RETURN = 267,
+    NONE = 268,
+    CONTINUE = 269,
+    FOR = 270,
+    LAMBDA = 271,
+    TRY = 272,
+    TRUE = 273,
+    DEF = 274,
+    FROM = 275,
+    NONLOCAL = 276,
+    WHILE = 277,
+    AND = 278,
+    DEL = 279,
+    GLOBAL = 280,
+    NOT = 281,
+    WITH = 282,
+    AS = 283,
+    ELIF = 284,
+    IF = 285,
+    OR = 286,
+    YIELD = 287,
+    ASSERT = 288,
+    ELSE = 289,
+    IMPORT = 290,
+    PASS = 291,
+    BREAK = 292,
+    EXCEPT = 293,
+    IN = 294,
+    RAISE = 295,
+    EXEC = 296,
+    NIGUAL = 297,
+    DIGUAL = 298,
+    MAIGUAL = 299,
+    MEIGUAL = 300,
+    MAYOR = 301,
+    MENOR = 302,
+    ENETILDE = 303,
+    CIRCUNFLEJO = 304,
+    PIPE = 305,
+    AMPERSON = 306,
+    DMAYOR = 307,
+    DMENOR = 308,
+    PORCEN = 309,
+    DDIAG = 310,
+    DIAG = 311,
+    DASTERISCO = 312,
+    ASTERISCO = 313,
+    MENOS = 314,
+    MAS = 315,
+    APAREN = 316,
+    CPAREN = 317,
+    ACORCHETE = 318,
+    CCORCHETE = 319,
+    ALLAVE = 320,
+    CLLAVE = 321,
+    COMA = 322,
+    DPUNTO = 323,
+    PUNTO = 324,
+    PCOMA = 325,
+    AT = 326,
+    IGUAL = 327,
+    MENOSMAYOR = 328,
+    MASIGUAL = 329,
+    MENOSIGUAL = 330,
+    ASTIGUAL = 331,
+    DIAIGUAL = 332,
+    DDIAIGUAL = 333,
+    PORIGUAL = 334,
+    AMPIGUAL = 335,
+    PIPEIGUAL = 336,
+    CIRCIGUAL = 337,
+    DMAYORIGUAL = 338,
+    DMENORIGUAL = 339,
+    DASTIGUAL = 340,
+    MENORMAYOR = 341,
+    CSIMPLE = 342,
+    IDIAG = 343,
+    GATO = 344,
+    DIDIAG = 345,
+    NEWLINE = 346,
+    ENTERO = 347,
+    FLOTANTE = 348,
+    INDENT = 349,
+    SALIDA = 350,
+    DEDENT = 351,
+    FIN = 352
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
+{
+#line 29 "pypar.y" /* yacc.c:1909  */
+
+  int numi;
+  float numf;
+  const char* cad;
+  char car;
+Node* nodeT;
+
+#line 166 "pypar.hpp" /* yacc.c:1909  */
+};
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_PYPAR_HPP_INCLUDED  */
